c4e5a3998afb8143c04fb7b3fe64c8f4c4e255fb
Sharat Cheerala <sharathsanthu@gmail.com>
2022-11-19 17:06:35 -0600

    Update streamlit_app.py
    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,11 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
+
+streamlit.text("Idly & Coconut Chutney")
+
+streamlit.text("Idly & Sambar")
+
+streamlit.text("Roti & Aloo Bhujiya")
+
+streamlit.text("Dosa & Peanut Chutney")
