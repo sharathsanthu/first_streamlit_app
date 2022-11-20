2aa18baffb12b5f5a9b5dcce0850b30453c77fce
Sharat Cheerala <sharathsanthu@gmail.com>
2022-11-20 15:18:06 -0600

    Update streamlit_app.py
    Importing data into a pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -14,5 +15,8 @@ streamlit.text("Dosa & Peanut Chutney")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
 
 
