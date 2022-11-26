e98e052ecbfa9687767464d3827ee96ee29fc7ac
Sharat Cheerala <sharathsanthu@gmail.com>
2022-11-25 22:00:03 -0600

    Update streamlit_app.py
    not pandas its pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 #New Section to display fruityvice api response 
 
